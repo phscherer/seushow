adaf590d4fe5273ca2de08d2ac47a15bf3358eed
Paulo Henrique Scherer <paulo.scherer@catolicasc.org.br>
2018-08-26 18:23:09 -0300

    Cria constantes para armazenar diametros dos cards

--- src/actionTypes/app.js
+++ src/actionTypes/app.js
@@ -1,2 +1,5 @@
 export const API_KEY = 'abbc8b6f414960dbb24217af0826cf11';
 export const IMAGE_PATH = 'https://image.tmdb.org/t/p/w500';
+export const PARALLAX_HEADER_HEIGHT = 315;
+export const STICKY_HEADER_HEIGHT = 40;
+export const AVATAR_SIZE = 120;
