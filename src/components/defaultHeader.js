2c367e77da233c79a4f51ce8478a93c155115554
Paulo Henrique Scherer <paulo.scherer@catolicasc.org.br>
2018-08-25 23:56:14 -0300

    Altera default header para permitir navegação entre telas

--- src/components/defaultHeader.js
+++ src/components/defaultHeader.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import { Header, Icon, Button, Title, Body, Right, Left } from 'native-base';
+import React from 'react';
+import { View, Header, Icon, Button, Title, Body, Right, Left, Tabs, Tab, Text } from 'native-base';
+import { withNavigation } from 'react-navigation';
+import MoviesList from '../containers/moviesList';
 
-class DefaultHeader extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Header hasTabs style={{ backgroundColor: '#AB3737' }}>
-        <Left style={{ flex: 1 }}>
-          <Button transparent>
-            <Icon type='EvilIcons' name='user' />
-          </Button>
-        </Left>
-        <Body style={{ flex: 1 }}>
-          <Title>{this.props.titlePage}</Title>
-        </Body>
-        <Right style={{ flex: 1 }}>
-          <Button transparent>
-            <Icon type='EvilIcons' name='search' />
-          </Button>
-        </Right>
-      </Header>
-    );
-  }
+const DefaultHeader = ({ titlePage, pageName, navigation }) => {
+  return (
+    <Header hasTabs style={{ backgroundColor: '#AB3737' }}>
+      <Left style={{ flex: 1 }}>
+        <Button transparent>
+          <Icon type='EvilIcons' name='user' />
+        </Button>
+      </Left>
+      <Body style={{ flex: 1 }}>
+        <Title>{ titlePage }</Title>
+      </Body>
+      <Right style={{ flex: 1 }}>
+        <Button transparent>
+          <Icon type='EvilIcons' name='search' />
+        </Button>
+      </Right>
+    </Header>
+  );
 }
 
-export default DefaultHeader;
+export default withNavigation(DefaultHeader);
