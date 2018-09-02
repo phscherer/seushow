ae1a623af7a72360686cf661584302cdb1d03ce8
Paulo Henrique Scherer <paulo.scherer@catolicasc.org.br>
2018-09-01 21:57:28 -0300

    Configura o link com o projeto react-native-fbsdk

--- android/app/src/main/java/com/selshow/MainApplication.java
+++ android/app/src/main/java/com/selshow/MainApplication.java
@@ -3,8 +3,11 @@ package com.selshow;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.oblador.vectoricons.VectorIconsPackage;
-
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -15,8 +18,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-
+  
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  
+  protected static CallbackManager getCallbackManager() {
+	  return mCallbackManager;
+  }
+  
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+	
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -27,7 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new FastImageViewPackage()
+            new FastImageViewPackage(),
+			new FBSDKPackage(mCallbackManager)
       );
     }
 
@@ -46,5 +57,8 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+	FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
   }
 }
