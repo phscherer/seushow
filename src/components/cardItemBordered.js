6389020ffc512f0d3b489cb1163ae3d929393a86
Paulo Henrique Scherer <paulo.scherer@catolicasc.org.br>
2018-09-16 12:39:52 -0300

    Cria modal para adicionar show a listas

--- src/components/cardItemBordered.js
+++ src/components/cardItemBordered.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import { Text, Container, Content, Card, CardItem, Body, Button, Icon } from 'native-base';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Modal from 'react-native-modal';
 import _ from 'lodash';
 import { cardButton } from '../styles/index';
 
 const styles = StyleSheet.create(cardButton);
+const window = Dimensions.get('window');
+
+const modalStyles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dialogStyle: {
+    height: window.height*0.4,
+    width: window.width*0.9,
+    backgroundColor: '#d3d3d3',
+    elevation: 10,
+    borderRadius: 5
+  },
+  anotherView: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
 
 class CardItemBordered extends Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      modalVisible: false,
+    }
+  }
+
+  setModalVisible = (condition) => {
+    this.setState({ modalVisible: condition });
+    console.log(this.state.modalVisible);
   }
 
   render() {
@@ -22,13 +54,17 @@ class CardItemBordered extends Component {
             <CardItem bordered>
               <Body>
                 <Text>
-                  {this.props.description === '' ? 'Sem informações.' : _.slice(this.props.description, 0, 700)}
+                  {this.props.show.overview === '' ? 'Sem informações.' : _.slice(this.props.show.overview, 0, 700)}
                 </Text>
               </Body>
             </CardItem>
           </Card>
           <View style={styles.buttonView}>
-            <Button success style={styles.button}>
+            <Button
+              success
+              style={styles.button}
+              onPress={() => this.setModalVisible(true)}
+            >
               <Icon
                 name='ios-add-circle'
                 style={styles.buttonIcon}
@@ -40,6 +76,20 @@ class CardItemBordered extends Component {
                 style={styles.buttonIcon}
               />
             </Button>
+          </View>
+          <View style={modalStyles.viewStyle}>
+            <Modal
+              animationType='slide'
+              transparent={true}
+              isVisible={this.state.modalVisible}
+              onBackdropPress={() => this.setModalVisible(false)}
+            >
+              <View style={modalStyles.viewStyle}>
+                <View style={modalStyles.dialogStyle}>
+                  
+                </View>
+              </View>
+            </Modal>
           </View>
         </Content>
       </Container>
