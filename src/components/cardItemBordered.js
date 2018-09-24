ca1b364e56b6b377164caf0806d867851768013c
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2018-09-24 13:32:25 -0300

    Cria funcionalidade para deletar shows

--- src/components/cardItemBordered.js
+++ src/components/cardItemBordered.js
@@ -11,6 +11,13 @@ import * as firebase from 'firebase';
 import b64 from 'base-64';
 import { cardButton } from '../styles/index';
 
+const showLists = [
+  'paraAssistir',
+  'ativas',
+  'favoritos',
+  'finalizadas',
+  'filmesAssistidos'
+];
 const styles = StyleSheet.create(cardButton);
 const window = Dimensions.get('window');
 
@@ -69,28 +76,41 @@ class CardItemBordered extends Component {
     let emailBase64 = b64.encode(user.email);
     let typeShow = !show.title ? 'serie' : 'movie';
     firebase.database()
-      .ref(`/users/${emailBase64}/${list}`)
+      .ref(`/users/${emailBase64}/${list}/${show.id}`)
       .push({
         showId: show.id,
         type: typeShow
       })
       .then(() => console.log('Success!'))
       .catch(error => this.setState({ errorMessage: error.message }));
-      let episodios = 0;
-      let qtdeShows = 0;
-      firebase.database()
-        .ref(`/users/${emailBase64}/`)
-        .on('value', snapshot => {
-          episodios = snapshot.val().episodiosAssistidos !== undefined ? snapshot.val().episodiosAssistidos : 0;
-          qtdeShows = snapshot.val().quantidadeShows !== undefined ? snapshot.val().quantidadeShows : 0;
-        });
-      firebase.database()
-        .ref(`/users/${emailBase64}/`)
-        .update({
-          episodiosAssistidos: episodios,
-          quantidadeShows: qtdeShows + 1
-        });
-      //this.setState({ modalVisible: false });
+    let episodios = 0;
+    let qtdeShows = 0;
+    firebase.database()
+      .ref(`/users/${emailBase64}/`)
+      .on('value', snapshot => {
+        episodios = snapshot.val().episodiosAssistidos !== undefined ? snapshot.val().episodiosAssistidos : 0;
+        qtdeShows = snapshot.val().quantidadeShows !== undefined ? snapshot.val().quantidadeShows : 0;
+      });
+    firebase.database()
+      .ref(`/users/${emailBase64}/`)
+      .update({
+        episodiosAssistidos: episodios,
+        quantidadeShows: qtdeShows + 1
+      });
+  }
+
+  disassociateShow = (show) => {
+    let user = firebase.auth().currentUser;
+    let emailBase64 = b64.encode(user.email);
+    let qtdeShows = 0;
+    firebase.database().ref(`/users/${emailBase64}/`)
+      .on('value', snapshot => {
+        qtdeShows = snapshot.val().quantidadeShows !== undefined ? snapshot.val().quantidadeShows : 0;
+      });
+    showLists.forEach((list) => {
+      firebase.database().ref(`/users/${emailBase64}/${list}/${show.id}`).remove();
+    });
+    firebase.database().ref(`/users/${emailBase64}/`).update({ quantidadeShows: qtdeShows - 1 });
   }
 
   render() {
@@ -121,7 +141,21 @@ class CardItemBordered extends Component {
                 style={styles.buttonIcon}
               />
             </Button>
-            <Button danger style={styles.button}>
+            <Button
+              danger
+              style={styles.button}
+              onPress={() => {
+                Alert.alert(
+                  'Desassociar show',
+                  'Tem certeza de que deseja desassociar o show de suas listas?',
+                  [
+                    { text: 'Cancelar', style: 'cancel' },
+                    { text: 'Ok', onPress: () => this.disassociateShow(show)},
+                  ],
+                  { cancelable: false }
+                );
+              }}
+            >
               <Icon
                 name='md-trash'
                 style={styles.buttonIcon}
